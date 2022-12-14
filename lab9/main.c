013922d1d55bb4889dfbf301cbdc87a4493db9e9
insp3ctah <113013232+insp3ctah@users.noreply.github.com>
2022-12-14 13:17:28 +0300

    lab 9

---
+++ lab9/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int ar[n+1];
+    for(int i=0;i<n+1;i++){
+        ar[i]=i;
+    }
+    for(int i=2;i<n+1;i++){
+        if(ar[i]!=0){
+            printf("%d\n",ar[i]);
+            for(int j = i*i;j<n+1;j+=i){
+                ar[j]=0;
+            }
+        }
+    }
+}
